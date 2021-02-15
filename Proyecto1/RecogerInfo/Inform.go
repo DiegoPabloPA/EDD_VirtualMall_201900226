43a7dad4ca5473cc6433cd17206b0e469675bdf1
Diego Pablo Pérez Alvarez <2963890720101@ingenieria.usac.edu.gt>
2021-02-15 01:11:18 -0600

    Fase de servidor
    Lectura y obtención de datos por medio de servidor go

--- Proyecto1/RecogerInfo/Inform.go
+++ Proyecto1/RecogerInfo/Inform.go
@@ -3,13 +3,40 @@ package Inform
 import (
 	vector "Proyecto1/ListaDVec"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-func TomarInfo(mensaje []byte)vector.InfoVector{
-	var deco vector.InfoVector
+var Informacion vector.InfoVector
 
-	if err := json.Unmarshal(mensaje, &deco); err != nil {
-		panic(err)
-	}
-	return deco
+func Path(){
+	router:=mux.NewRouter()
+	router.HandleFunc("/",PaginaInicio)
+	router.HandleFunc("/cargartienda",AsignarInformacion).Methods("POST")
+	router.HandleFunc("/mostrar",Mostrar)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
+
+func PaginaInicio(w http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(w,"TODOS LOS DERECHOS RESERVADOS")
+}
+
+func Mostrar(w http.ResponseWriter, req *http.Request){
+
+	if len(Informacion.Indice1)==0{
+		fmt.Fprintf(w,"Error aun no se han cargado datos")
+	}
+}
+
+
+
+func AsignarInformacion(w http.ResponseWriter, req *http.Request){
+	respuesta,_:=ioutil.ReadAll(req.Body)
+
+	if err := json.Unmarshal(respuesta, &Informacion); err != nil {
+		panic(err)
+
+	}}
--- Proyecto1/main.go
+++ Proyecto1/main.go
@@ -1,9 +1,14 @@
 package main
 
+import Inform "Proyecto1/RecogerInfo"
+
 func main() {
-
-
-
-
+Inform.Path()
 
 }
+
+
+
+
+
+
